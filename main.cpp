b42f2c7b2b778a2ca1350713bc7d3e328ba1651c
H3w9VH3F4L4Tq5MbsfRi <rizezir@gmail.com>
2024-12-13 21:52:40 +0100

    Change project name

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(width, height, "Name", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(width, height, "Kinematic chain 2D", NULL, NULL);
     if (window == NULL) {
       std::cout << "Failed to create GLFW window" << std::endl;
       glfwTerminate();
